01687b4dee29df1465f7473b1d3a7c7ba07fbc73
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-20 23:37:04 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ class $modify(MyMenuLayer, MenuLayer) {
 
 class $modify(GameManager) {
     void fadeInMenuMusic() {
-        auto setting = Mod::get()->getSettingV3("utMusic");
+        auto setting = Mod::get()->getSettingValue("utMusic");
         if (setting && setting->get<bool>() && !getGameVariable("0122")) {
             FMODAudioEngine::sharedEngine()->playMusic("menu.ogg"_spr, true, 0.0f, 0);
         } else {
@@ -66,7 +66,7 @@ class $modify(GameManager) {
     }
 
     void playMenuMusic() {
-        auto setting = Mod::get()->getSettingV3("utMusic");
+        auto setting = Mod::get()->getSettingValue("utMusic");
         if (setting && setting->get<bool>() && !getGameVariable("0122")) {
             FMODAudioEngine::sharedEngine()->playMusic("menu.ogg"_spr, true, 0.0f, 0);
         } else {
@@ -89,7 +89,8 @@ class $modify(LoadingLayer) {
     bool init(bool p0) {
         if (!LoadingLayer::init(p0)) return false;
 
-        auto setting = Mod::get()->getSettingV3("loadingBar");
+        auto setting = Mod::get()->getSettingValue
+alue("loadingBar");
         bool barVisible = setting && setting->get<bool>();
 
         CCObject* obj;
